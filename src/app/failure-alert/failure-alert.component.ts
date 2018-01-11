8847f352d7b8305b32e4df985ecf92cab3d5a26b
Algebrah <JWCarroll88@gmail.com>
2018-01-11 09:14:32 -0600

    Initial Commit

---
+++ src/app/failure-alert/failure-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'failure-alert',
+  templateUrl: './failure-alert.component.html',
+  styleUrls: ['./failure-alert.component.css']
+})
+export class FailureAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
